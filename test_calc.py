b9768cdc3520b9b5f79d2cd74e3fb3749f120dc7
pgmystery <philipp.glaw@gmx.de>
2025-03-19 12:01:05 +0100

    Add more tests.

--- test_calc.py
+++ test_calc.py
@@ -7,16 +7,19 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(add(3, 2), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        self.assertRaises(TypeError, add, ("nan", 5))
 
     def test_subtract(self):
         self.assertEqual(subtract(3, 2), 1)
         self.assertEqual(subtract(-1, 1), -2)
         self.assertEqual(subtract(0, 0), 0)
+        self.assertRaises(TypeError, subtract, (5, []))
 
     def test_multiply(self):
         self.assertEqual(multiply(3, 2), 6)
         self.assertEqual(multiply(-1, 1), -1)
         self.assertEqual(multiply(0, 0), 0)
+        self.assertRaises(TypeError, multiply, (5, True))
 
 
 if __name__ == "__main__":
